160651dcebbe8771adc231fa5caccdc25e539563
yasinasama <614867000@qq.com>
2019-02-20 22:31:29 +0800

    update

--- msql/client.py
+++ msql/client.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from itertools import groupby
-
 import pymongo
-from bson.objectid import ObjectId
 
 from .grammar import parse_handle
+from .find import Find
+from .limit import Limit
+from .sort import Sort
 
 
 class Mongo:
@@ -15,107 +15,6 @@ class Mongo:
 
         self.mongo = pymongo.MongoClient(host, port)
         self.db = self.mongo[db]
-
-        self.asc = pymongo.ASCENDING
-        self.desc = pymongo.DESCENDING
-         
-        self.result = None
-
-    # def objectid_to_str(self):
-
-    def _compare_mapping(self,comp):
-        if comp == '>':
-            return '$gt'
-        if comp == '>=':
-            return '$gte'
-        if comp == '<':
-            return '$lt'
-        if comp == '<=':
-            return '$lte'
-        if comp == '!=':
-            return '$ne'
-
-    def _sort_mapping(self,sort):
-        return self.asc if sort=='ASC' else self.desc
-
-    def _split_list(self,source,wd):
-        return [list(g) for k, g in groupby(source, lambda x: x == wd) if not k]
-
-
-    def _columns(self,columns):
-        c = {}
-        need_id = False
-        for column in columns:
-            n = column['name']
-            if n == '*':
-                c = {}
-                break
-            if n == 'id':
-                need_id = True
-            else:
-                c[n] = 1
-        if not need_id:
-            c['_id'] = 0
-        return c
-
-    def _table(self,table):
-        self.table = self.db[table]
-
-    def _where(self,where,column):
-        d = {}
-
-        if where:
-            d = self._make_condsl(where)
-        c = self._columns(column)
-        self.result = self.table.find(d,c)
-            
-    def _make_condsl(self,conditions):
-        condsl = {}
-        # filter 
-        while isinstance(conditions, list) and len(conditions) == 1:
-            conditions = conditions[0]
-
-        if isinstance(conditions, list):
-            # `AND` has high priority,so we should split `OR` first
-            if 'OR' in conditions:
-                k,b = '$or','OR'
-            else:
-                k,b = '$and','AND'
-
-            subconditions = self._split_list(conditions,b)
-            for sub in subconditions:
-                condsl.setdefault(k,[]).append(self._make_condsl(sub))
-        else:
-            name, value, comp = conditions['name'], conditions['value'], conditions['compare']
-            if comp in ('>', '>=', '<', '<=','!='):
-                condsl[name] = {self._compare_mapping(comp): value}
-            elif comp == '=':
-                condsl[name] = value
-            elif comp == 'LIKE':
-                if not value.startswith('%'):
-                    value = '^' + value
-                if not value.endswith('%'):
-                    value += '$'
-                regex = value.strip('%').replace('%','*')
-                condsl[name] = {'$regex':regex}
-        print(condsl)
-        return condsl
-
-    def _order(self,order):
-        d = []
-        for o in order:
-            name,value = o['name'],o['type']
-            d.append((name,self._sort_mapping(value)))
-        self.reslut = self.result.sort(d)
-
-    def _limit(self,limit):
-        self.reslut = self.result.limit(limit)
-
-    def _skip(self,skip):
-        self.reslut = self.result.skip(skip)
-
-    def _map(self):
-        pass
 
     def execute(self,sql):
         sql = sql.strip(';')+';'
@@ -131,18 +30,21 @@ class Mongo:
         order = dsl['order']
         limit = dsl['limit']
 
-        self._table(table)
-
-        self._where(where,column)
+        res = self.db[table]
+        if where:
+            res = res.find(*Find(column,where).find())
 
         if order:
-            self._order(order)
+            res = res.sort(Sort(order).sort())
 
         if limit:
-            self._limit(limit[1])
-            self._skip(limit[0])
+            s,l = Limit(limit).limit()
+            if s:
+                res = res.limit(l)
+            if l:
+                res = res.skip(s)
 
-        return self.result
+        return res
 
 
 
--- tests/msql_test.py
+++ tests/msql_test.py
@@ -4,6 +4,5 @@ import msql
 
 if __name__=='__main__':
     mg = msql.Mongo('127.0.0.1',27017,'cj')
-    for i in mg.execute('select userid from user where userid like "a%"'):
+    for i in mg.execute('select userid from user where userid like "a%" limit 1'):
         print(i)
-    # print(len(mg.execute()))
